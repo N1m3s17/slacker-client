29698410790b69d93262ac4d409066bb3e9bc94c
N1m3s17 <seminspatel@gmail.com>
2018-12-15 21:11:39 +0000

    modifying slacker code

--- scripts/profile.js
+++ scripts/profile.js
@@ -11,48 +11,6 @@ var user = {
  }
 };
 
-var messages = {
- {
- "user": {
- "name": "Kat",
- "image": "https://images.pexels.com/photos/415829/pexels-photo415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=25"
- },
- "message": {
- "text": "I'm changing the due date on the landing page because I need more time. The designs look great!",
- "date": 1534885670000
- }
-}
- {
- "user": {
- "name": "Ron",
- "image": "https://images.pexels.com/photos/1498335/pexels-photo1498335.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=25"
-},
- "message": {
- "text": "I'm changing the due date on the landing page because I need more time. The designs look great!",
- "date": 1534485670000
- }
-}
- {
- "user": {
- "name": "Benny",
- "image": "https://images.pexels.com/photos/614810/pexels-photo614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=25"
- },
- "message": {
- "text": "It is great to finally see this project complete",
- "date": 1534785670000
- }
-}
- {
- "user": {
- "name": "Betty",
- "image": "https://images.pexels.com/photos/1498778/pexels-photo1498778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=25"
- },
- "message": {
- "text": "Next time I will be able to see things better",
- "date": 1537564070000
- }
-}
-
 document.title = user['user']['full-name'];
 
 document.getElementById("profile_pic").src = user['user']['image'];
